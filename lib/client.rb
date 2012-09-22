4829105e8bbd3dcf158c1e2934e474ef8e141351
Chiel Kunkels <chiel@kunkels.me>
2012-09-22 20:18:25 +0100

    Added some basic auth with github

--- lib/client.rb
+++ lib/client.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
+require 'omniauth'
+require 'omniauth-github'
 
 CONF = JSON.parse File.open('config/config.json', 'rb').read
 
 class Client < Sinatra::Base
   set :root, File.expand_path('../..', __FILE__)
+  use Rack::Session::Cookie
+  use OmniAuth::Builder do
+    provider :github, CONF['auth']['github']['id'], CONF['auth']['github']['secret']
+  end
 
   get '/' do
     locals = {
@@ -16,6 +22,22 @@ class Client < Sinatra::Base
       }
     }
     erb :index, :locals => locals
+  end
+
+  get '/auth/:provider/callback' do
+    email = request.env['omniauth.auth'][:info][:email]
+    session[:user] = {
+      :email => email
+    }
+    redirect request.env['omniauth.origin'] || '/'
+  end
+
+  get '/auth/failure' do
+    delete session[:user]
+  end
+
+  get '/logout' do
+    delete session[:user]
   end
 end
 
